a5652168fe9e5d5bf3e8f988ee4127b52f32eaaa
John Hanbury <johnhanbury4@gmail.com>
2019-10-04 01:21:53 +0100

    updated database models for work experience, update mappings and habdler for experience, updated time line ui for work experience

---
+++ Shared/Site.Application/Entities/University.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Site.Application.Entities
+{
+    public class University
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string  ShortName { get; set; }
+        public string  City { get; set; }
+        public string  Country { get; set; }
+        public ICollection<Degree> Degrees { get; set; }
+    }
+}

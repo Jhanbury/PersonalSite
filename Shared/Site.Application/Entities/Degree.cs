027fcb6a16c423adbd1d79cf6c0839b93e1aa30d
John Hanbury <johnhanbury4@gmail.com>
2019-10-04 01:21:53 +0100

    updated database models for work experience, update mappings and habdler for experience, updated time line ui for work experience

---
+++ Shared/Site.Application/Entities/Degree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Site.Application.Entities
+{
+    public class Degree
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int UniversityId { get; set; }
+        public int GradeId { get; set; }
+        public University University { get; set; }
+        public Grade Grade { get; set; }
+        public User User { get; set; }
+
+    }
+}
---
+++ Shared/Site.Application/Entities/Grade.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Site.Application.Entities
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string UsGrade { get; set; }
+        public ICollection<Degree> Degrees { get; set; }
+        
+    }
+}

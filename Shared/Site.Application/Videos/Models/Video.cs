48123341d7707248d91b20ed384cff79fe851d3f
John Hanbury <johnhanbury4@gmail.com>
2019-11-16 23:55:55 +0000

    added video and platform account table, added youtube integration service

---
+++ Shared/Site.Application/Videos/Models/Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Site.Application.PlatformAccounts.Model;
+
+namespace Site.Application.Videos.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string Url { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int PlatformAccountId { get; set; }
+        public string SourceId { get; set; }
+        public string VideoDuration { get; set; }
+        public PlatformAccount PlatformAccount { get; set; }
+    }   
+}

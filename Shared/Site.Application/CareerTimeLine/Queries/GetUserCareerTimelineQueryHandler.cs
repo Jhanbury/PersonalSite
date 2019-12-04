bc13d4412ca8ecc2c43a028d44b679cc9f2b3ce8
John Hanbury <johnhanbury4@gmail.com>
2019-12-04 01:01:56 +0000

    added user career Timeline to project

--- Shared/Site.Application/CareerTimeLine/Queries/GetUserCareerTimelineQueryHandler.cs
+++ Shared/Site.Application/CareerTimeLine/Queries/GetUserCareerTimelineQueryHandler.cs
@@ -12,20 +12,43 @@ namespace Site.Application.CareerTimeLine.Queries
 {
   public class GetUserCareerTimelineQueryHandler : IRequestHandler<GetUserCareerTimelineQuery, List<CareerTimeLineDto>>
   {
-    private readonly IRepository<UserCertification, int> _repository;
+    private readonly IRepository<UserCertification, int> _certRepository;
+    private readonly IRepository<UserDegree, int> _degreeRepository;
     private readonly IMapper _mapper;
 
-    public GetUserCareerTimelineQueryHandler(IRepository<UserCertification, int> repository, IMapper mapper)
+    public GetUserCareerTimelineQueryHandler(IRepository<UserCertification, int> repository, IRepository<UserDegree, int> degreeRepository, IMapper mapper)
     {
       _mapper = mapper;
-      _repository = repository;
+      _certRepository = repository;
+      _degreeRepository = degreeRepository;
 
     }
 
+    
+
     public async Task<List<CareerTimeLineDto>> Handle(GetUserCareerTimelineQuery request, CancellationToken cancellationToken)
     {
-      var certifications = await _repository.GetIncluding(x => x.UserId.Equals(request.UserId), x => x.Certification, x => x.Certification.Accreditor);
+      var userCertsTask = GetUserCerts(request.UserId);
+      var userDegreesTask = GetUserDegrees(request.UserId);
+
+      await Task.WhenAll(userCertsTask, userDegreesTask);
+      var userDegrees = userDegreesTask.Result;
+      var userCerts = userCertsTask.Result;
+
+      return userDegrees.Union(userCerts).OrderBy(x => x.Date).ToList();
+    }
+
+
+    private async Task<IEnumerable<CareerTimeLineDto>> GetUserCerts(int id)
+    {
+      var certifications = await _certRepository.GetIncluding(x => x.UserId.Equals(id), x => x.Certification, x => x.Certification.Accreditor);
       return certifications.Select(x => _mapper.Map<CareerTimeLineDto>(x)).OrderBy(x => x.Date).ToList();
+    }
+
+    private async Task<IEnumerable<CareerTimeLineDto>> GetUserDegrees(int id)
+    {
+      var degrees = await _degreeRepository.GetIncluding(x => x.UserId.Equals(id), x => x.Degree, x => x.Degree.University, x => x.Grade);
+      return degrees.Select(x => _mapper.Map<CareerTimeLineDto>(x)).OrderBy(x => x.Date).ToList();
     }
   }
 }

02b8d48651ca00232f2c29d29f859d882caae0b0
John Hanbury <johnhanbury4@gmail.com>
2019-12-18 19:35:39 +0000

    removed unnesscary props from user model, resolve #30

--- Shared/Site.Application/Users/Models/UserDto.cs
+++ Shared/Site.Application/Users/Models/UserDto.cs
@@ -4,11 +4,8 @@ namespace Site.Application.Users.Models
 {
     public class UserDto
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string Email { get; set; }
         public string  CurrentLocation { get; set; }
         public string PersonalStatement { get; set; }
         

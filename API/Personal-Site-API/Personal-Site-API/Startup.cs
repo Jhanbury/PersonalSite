4e65e56cdd363d71dca3fca0bf97a29503901967
John Hanbury <johnhanbury4@gmail.com>
2020-03-22 21:53:35 +0000

    cleanup start up

--- API/Personal-Site-API/Personal-Site-API/Startup.cs
+++ API/Personal-Site-API/Personal-Site-API/Startup.cs
@@ -27,118 +27,102 @@ using Site.Persistance;
 namespace Personal_Site_API
 {
   public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-        public IContainer ApplicationContainer { get; set; }
-        public IConfiguration Configuration { get; }
+      Configuration = configuration;
+    }
+    public IContainer ApplicationContainer { get; set; }
+    public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            var dbConnectionString = Configuration["ConnectionString"];
-            var devToAPIKey = Configuration["DevtoAPIKey"];
-            ConfigureCaching(services, dbConnectionString);
-            ConfigureHttpSettings(services);
-            ConfigureAutoMapper(services);
-            ConfigureMediatR(services);
-            ConfigureHttpClientFactory(services, devToAPIKey);
-            ConfigureSerilog(dbConnectionString);
-            ConfigureHangFire(services, dbConnectionString);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddCors(options =>
-            {
-                options.AddPolicy("cors",
-                    builder =>
-                    {
-                        builder.AllowAnyHeader()
-                            .AllowAnyOrigin()
-                            .AllowAnyMethod();
-                    });
-            });
-            
-            
-            return new Container().WithDependencyInjectionAdapter(services)
-              .ConfigureServiceProvider<CompositionRoot>();
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public IServiceProvider ConfigureServices(IServiceCollection services)
+    {
+      var dbConnectionString = Configuration["ConnectionString"];
+      var devToAPIKey = Configuration["DevtoAPIKey"];
+      ConfigureCaching(services, dbConnectionString);
+      ConfigureHttpSettings(services);
+      ConfigureAutoMapper(services);
+      ConfigureMediatR(services);
+      ConfigureHttpClientFactory(services, devToAPIKey);
+      ConfigureSerilog(dbConnectionString);
+      ConfigureHangFire(services, dbConnectionString);
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddCors(options =>
+      {
+        options.AddPolicy("cors",
+                  builder =>
+                  {
+                builder.AllowAnyHeader()
+                          .AllowAnyOrigin()
+                          .AllowAnyMethod();
+              });
+      });
+      
+      return new Container().WithDependencyInjectionAdapter(services)
+        .ConfigureServiceProvider<CompositionRoot>();
 
-            
-        }
+    }
 
-        private void ConfigureHttpSettings(IServiceCollection services)
-        {
-          services.AddHttpClient();
-          services.AddHttpContextAccessor();
-        }
+    private void ConfigureHttpSettings(IServiceCollection services)
+    {
+      services.AddHttpClient();
+      services.AddHttpContextAccessor();
+    }
 
+    private void ConfigureCaching(IServiceCollection services, string connectionString)
+    {
+      services.AddDistributedMemoryCache();
+      services.AddSingleton<ICache>(sp =>
+          new FluentIDistributedCache(sp.GetService<IDistributedCache>(), new Serializer()));
+      services.AddDbContext<SiteDbContext>(options => options.UseSqlServer(connectionString));
+    }
 
-        private void ConfigureCaching(IServiceCollection services, string connectionString)
-        {
-            services.AddDistributedMemoryCache();
-            services.AddSingleton<ICache>(sp =>
-                new FluentIDistributedCache(sp.GetService<IDistributedCache>(), new Serializer()));
-            services.AddDbContext<SiteDbContext>(options => options.UseSqlServer(connectionString));
-        }
+    private void ConfigureHttpClientFactory(IServiceCollection services, string blogAPIKey)
+    {
+      services.AddHttpClient("dev.to", client =>
+      {
+        client.BaseAddress = new Uri("https://dev.to/");
+        client.DefaultRequestHeaders.Add("api-key", blogAPIKey);
 
-        private void ConfigureHttpClientFactory(IServiceCollection services, string blogAPIKey)
-        {
-            services.AddHttpClient("dev.to", client =>
-            {
-              client.BaseAddress = new Uri("https://dev.to/");
-              client.DefaultRequestHeaders.Add("api-key", blogAPIKey);
-              
-            });
-            services.AddHttpClient("github", client =>
-            {
-              client.BaseAddress = new Uri("https://api.github.com");
-              client.DefaultRequestHeaders.Add("User-Agent", "Personal-Site");
-            });
+      });
+      services.AddHttpClient("github", client =>
+      {
+        client.BaseAddress = new Uri("https://api.github.com");
+        client.DefaultRequestHeaders.Add("User-Agent", "Personal-Site");
+      });
+    }
 
+    private void ConfigureAutoMapper(IServiceCollection services)
+    {
+      services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).GetTypeInfo().Assembly, typeof(InfrastructureProfile).GetTypeInfo().Assembly });
+    }
 
-     
-        }
+    public void ConfigureMediatR(IServiceCollection services)
+    {
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPreProcessorBehavior<,>));
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+      services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
 
-        private void ConfigureAutoMapper(IServiceCollection services)
-        {
-            services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).GetTypeInfo().Assembly, typeof(InfrastructureProfile).GetTypeInfo().Assembly });
-        }
+      services.AddMediatR(typeof(GetAllGithubReposQuery).GetTypeInfo().Assembly);
+    }
 
-        public void ConfigureMediatR(IServiceCollection services)
-        {
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPreProcessorBehavior<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+    public void ConfigureSerilog(string connectionString)
+    {
+      Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Information()
+          .Enrich.FromLogContext()
+          .Enrich.WithProperty("Application", "API")
+          .WriteTo.MSSqlServer(connectionString, "Logs")
+          .CreateLogger();
+    }
 
-            services.AddMediatR(typeof(GetAllGithubReposQuery).GetTypeInfo().Assembly);
-        }
+    public void ConfigureHangFire(IServiceCollection services, string connectionString)
+    {
+      services.AddHangfire(x => x.UseSqlServerStorage(connectionString));
+      services.AddHangfireServer();
+      JobStorage.Current = new SqlServerStorage(connectionString);
 
-        public void ConfigureSerilog(string connectionString)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .Enrich.WithProperty("Application", "API")
-                .WriteTo.MSSqlServer(connectionString, "Logs")
-                .CreateLogger();
-        }
-
-        public void ConfigureHangFire(IServiceCollection services, string connectionString)
-        {
-            services.AddHangfire(x => x.UseSqlServerStorage(connectionString));
-            services.AddHangfireServer();
-            JobStorage.Current = new SqlServerStorage(connectionString);
-
-      //BackgroundJob.Enqueue<ITwitchService>(service => service.UpdateTwitchAccounts(1));
-      //BackgroundJob.Enqueue<IYouTubeService>(service => service.UpdateYouTubeAccounts(1));
-      //BackgroundJob.Enqueue<IRecurringJobService>(service => service.UpdateUserBlogs(1));
-      //BackgroundJob.Enqueue<IRecurringJobService>(service => service.UpdateGithubRepos(1, "Jhanbury"));
-      //BackgroundJob.Enqueue<IRecurringJobService>(service => service.UpdateVideoPlatforms(1));
-      //BackgroundJob.Enqueue<IRecurringJobService>(service => service.SubscribeToTwitchWebhooks(1));
-      //BackgroundJob.Enqueue<IRecurringJobService>(service => service.UpdateUserBlogs(1));
-      //BackgroundJob.Enqueue<IRecurringJobService>(service => service.UpdateUserBlogs(1));
-      //BackgroundJob.Enqueue<IBlogPostService>(service => service.UpdateBlogPostsForUser(1));
       RecurringJob.AddOrUpdate<IRecurringJobService>(service => service.UpdateGithubRepos(1, "JHanbury"), Cron.Daily);
       RecurringJob.AddOrUpdate<IRecurringJobService>(service => service.UpdateUserBlogs(1), Cron.Daily);
       RecurringJob.AddOrUpdate<IRecurringJobService>(service => service.UpdateVideoPlatforms(1), Cron.Daily);
@@ -146,26 +130,24 @@ namespace Personal_Site_API
 
     }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseHsts();
+      }
 
-            
-            app.UseCors("cors");
-            app.UseHttpsRedirection();
-            app.UseHangfireServer();
-            app.UseHangfireDashboard();
-            //app.UseAuthentication();
-            app.UseMvc();
-        }
+
+      app.UseCors("cors");
+      app.UseHttpsRedirection();
+      app.UseHangfireServer();
+      app.UseHangfireDashboard();
+      app.UseMvc();
     }
+  }
 }

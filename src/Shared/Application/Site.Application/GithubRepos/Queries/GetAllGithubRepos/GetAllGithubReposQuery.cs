a25c39c60799622dbc22488b86982097267d0a10
John Hanbury <johnhanbury4@gmail.com>
2020-09-12 15:41:38 +0100

    added wo endpoints to endpoints function

--- src/Shared/Application/Site.Application/GithubRepos/Queries/GetAllGithubRepos/GetAllGithubReposQuery.cs
+++ src/Shared/Application/Site.Application/GithubRepos/Queries/GetAllGithubRepos/GetAllGithubReposQuery.cs
@@ -4,7 +4,7 @@ using Site.Application.GithubRepos.Models;
 
 namespace Site.Application.GithubRepos.Queries.GetAllGithubRepos
 {
-    public class GetAllGithubReposQuery : IRequest<GithubRepoDto>, IRequest<List<GithubRepoDto>>
+    public class GetAllGithubReposQuery : IRequest<List<GithubRepoDto>>
     {
     public int UserId { get; set; }
 

3aa01b17ed49c6b505cf74f7b28de5473c317778
John Hanbury <johnhanbury4@gmail.com>
2020-09-14 12:43:29 +0100

    #45
    migrated apis to functions,
    updated twitch webhook

--- src/Shared/Application/Site.Application/PlatformAccounts/Commands/UpdateAccountStreamStateCommand.cs
+++ src/Shared/Application/Site.Application/PlatformAccounts/Commands/UpdateAccountStreamStateCommand.cs
@@ -2,7 +2,7 @@ using MediatR;
 
 namespace Site.Application.PlatformAccounts.Commands
 {
-  public class UpdateAccountStreamStateCommand : IRequest, IRequest<bool>
+  public class UpdateAccountStreamStateCommand : IRequest<bool>
   {
     public string AccountId { get; set; }
     public bool IsStreaming { get; set; }

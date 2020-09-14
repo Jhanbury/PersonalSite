3112819538eaf9e4fc3bb0765e3b7617f9f187a6
John Hanbury <johnhanbury4@gmail.com>
2020-09-14 22:36:00 +0100

    added worker project, ported over config, added extensions for common config

---
+++ src/Cloud/Functions/Worker/Site.Worker/Site.Worker/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using AutoMapper;
+using MediatR;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.DependencyInjection;
+using Site.Application.Infrastructure.AutoMapper;
+using Site.Application.Interfaces;
+using Site.Application.Interfaces.Messaging;
+using Site.Application.Users.Queries;
+using Site.Infrastructure;
+using Site.Infrastructure.MessageHandlers;
+using Site.Infrastructure.Modules;
+using Site.Infrastructure.Services;
+using Site.Persistance;
+using Site.Persistance.Repository;
+
+[assembly: FunctionsStartup(typeof(Site.Worker.Startup))]
+namespace Site.Worker
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var localConfig = GetLocalConfiguration();
+            var uri = localConfig.GetValue<string>("VaultUri");
+            builder
+                .ConfigureKeyVault(uri)
+                .ConfigureServices()
+                .ConfigureMessagingHandlers();
+
+        }
+
+        private static IConfiguration GetLocalConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddUserSecrets(typeof(Startup).Assembly)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}

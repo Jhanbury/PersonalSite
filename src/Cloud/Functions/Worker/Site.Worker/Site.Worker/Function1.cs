fc312cc72fe74ea1b581327f696c5946eaf4b248
John Hanbury <johnhanbury4@gmail.com>
2020-09-14 22:36:00 +0100

    added worker project, ported over config, added extensions for common config

--- src/Cloud/Functions/Worker/Site.Worker/Site.Worker/Function1.cs
+++ src/Cloud/Functions/Worker/Site.Worker/Site.Worker/Function1.cs
@@ -1,6 +1,6 @@
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -10,17 +10,17 @@ using Site.Infrastructure.Messages;
 
 namespace Site.Worker
 {
-    public class RunJob
+    public class Worker
     {
         private readonly IMessageHandlerFactory _messageHandlerFactory;
 
-        public RunJob(IMessageHandlerFactory messageHandlerFactory)
+        public Worker(IMessageHandlerFactory messageHandlerFactory)
         {
             _messageHandlerFactory = messageHandlerFactory;
         }
 
-        [FunctionName("RunJob")]
-        public async Task Run([ServiceBusTrigger("jobs", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
+        [FunctionName("Worker")]
+        public async Task HandleJob([ServiceBusTrigger("jobs", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
         {
             var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, NullValueHandling = NullValueHandling.Ignore };
             var message = JsonConvert.DeserializeObject<Message>(myQueueItem, settings);

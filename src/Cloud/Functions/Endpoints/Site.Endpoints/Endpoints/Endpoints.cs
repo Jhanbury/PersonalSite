4b16a017b069fa6fc401ebbf91b7b095b9b09130
John Hanbury <johnhanbury4@gmail.com>
2020-09-14 12:43:29 +0100

    #45
    migrated apis to functions,
    updated twitch webhook

--- src/Cloud/Functions/Endpoints/Site.Endpoints/Endpoints/Endpoints.cs
+++ src/Cloud/Functions/Endpoints/Site.Endpoints/Endpoints/Endpoints.cs
@@ -9,18 +9,27 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using MediatR;
+using Site.Application.BlogPosts.Queries.GetUserBlogPosts;
 using Site.Application.GithubRepos.Queries.GetAllGithubRepos;
+using Site.Application.Hobbies.Querys;
+using Site.Application.Infrastructure.Models.Twitch;
+using Site.Application.Interfaces;
+using Site.Application.Projects.Queries;
 using Site.Application.SocialMediaAccounts.Queries;
+using Site.Application.Users.Queries;
+using Site.Application.Videos.Queries;
 
 namespace Endpoints
 {
     public class Endpoints
     {
         private readonly IMediator _mediator;
+        private readonly ITwitchService _twitchService;
 
-        public Endpoints(IMediator mediator)
+        public Endpoints(IMediator mediator, ITwitchService twitchService)
         {
             _mediator = mediator;
+            _twitchService = twitchService;
         }
 
         [FunctionName("SocialLinks")]
@@ -41,6 +50,74 @@ namespace Endpoints
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             var result = await _mediator.Send(new GetAllGithubReposQuery(id));
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("Projects")]
+        public async Task<IActionResult> Projects([HttpTrigger(AuthorizationLevel.Function, "get", Route = "user/{id}/projects")] HttpRequest req, int id,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var result = await _mediator.Send(new GetUserProjectsQuery(id));
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("About")]
+        public async Task<IActionResult> About([HttpTrigger(AuthorizationLevel.Function, "get", Route = "user/{id}/about")] HttpRequest req, int id,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var result = await _mediator.Send(new GetUserInfoQuery(id));
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("Blogs")]
+        public async Task<IActionResult> Blogs([HttpTrigger(AuthorizationLevel.Function, "get", Route = "user/{id}/blogs")] HttpRequest req, int id,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var result = await _mediator.Send(new GetUserBlogPostsQuery(id));
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("Videos")]
+        public async Task<IActionResult> Videos([HttpTrigger(AuthorizationLevel.Function, "get", Route = "user/{id}/videos")] HttpRequest req, int id,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var result = await _mediator.Send(new GetAllUserVideos(id));
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("Hobbies")]
+        public async Task<IActionResult> Hobbies([HttpTrigger(AuthorizationLevel.Function, "get", Route = "user/{id}/hobbies")] HttpRequest req, int id,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var result = await _mediator.Send(new GetUserHobbiesQuery(id));
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("TwitchStream")]
+        public async Task<IActionResult> TwitchStream([HttpTrigger(AuthorizationLevel.Function, "post", Route = "twitch/streamupdate/{userId}/{accountId}")] HttpRequest req, int userId, string accountId,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<TwitchStreamUpdateResponse>(requestBody);
+            var commandData = _twitchService.HandleTwitchStreamUpdateWebhook(data, userId, accountId);
+            var result = await _mediator.Send(commandData);
 
             return new OkObjectResult(result);
         }

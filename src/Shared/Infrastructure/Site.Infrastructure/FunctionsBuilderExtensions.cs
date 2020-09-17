6f719df4ef7f71b1854536d2f0832260faa6d8a3
John Hanbury <johnhanbury4@gmail.com>
2020-09-17 09:54:16 +0100

    added tests to scheduler solution

--- src/Shared/Infrastructure/Site.Infrastructure/FunctionsBuilderExtensions.cs
+++ src/Shared/Infrastructure/Site.Infrastructure/FunctionsBuilderExtensions.cs
@@ -55,6 +55,20 @@ namespace Site.Infrastructure
             return builder;
         }
 
+
+        public static IFunctionsHostBuilder ConfigureSchedulerServices(this IFunctionsHostBuilder builder, string connectionString)
+        {
+            builder.Services.AddHttpClient();
+            builder.Services.AddDbContext<SiteDbContext>(options => options.UseSqlServer(connectionString));
+            builder.Services.AddScoped(typeof(IRepository<,>), typeof(EFRepository<,>));
+            builder.Services.AddScoped<IRecurringJobService, RecurringJobService>();
+            builder.Services.AddTransient<IGithubService, GithubRepoServices>();
+            builder.Services.AddTransient<IBlogPostService, BlogPostService>();
+            builder.Services.AddTransient<ITwitchService, TwitchService>();
+            builder.Services.AddTransient<IYouTubeService, YouTubeService>();
+            return builder;
+        }
+
         public static IFunctionsHostBuilder ConfigureMessagingHandlers(this IFunctionsHostBuilder builder)
         {
             builder.Services.AddScoped<HandlerResolver>(sp => message =>
@@ -95,7 +109,9 @@ namespace Site.Infrastructure
                 new KeyVaultClient.AuthenticationCallback(
                     azureServiceTokenProvider.KeyVaultTokenCallback));
 
-            var config = new ConfigurationBuilder().AddAzureKeyVault(vaultUrl, keyVaultClient, new DefaultKeyVaultSecretManager()).Build();
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddAzureKeyVault(vaultUrl, keyVaultClient, new DefaultKeyVaultSecretManager()).Build();
 
             builder.Services.AddSingleton<IConfiguration>(config);
             return builder;

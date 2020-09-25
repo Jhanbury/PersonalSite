a213b3f9ae3c021fbf78cbe03719438eaefd966f
John Hanbury <johnhanbury4@gmail.com>
2020-09-25 12:24:28 +0100

    added camel case serializer settings

--- src/Cloud/Functions/Endpoints/Site.Endpoints/Endpoints/Startup.cs
+++ src/Cloud/Functions/Endpoints/Site.Endpoints/Endpoints/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Site.Application.Infrastructure.AutoMapper;
 using Site.Application.Interfaces;
 using Site.Application.Users.Queries;
@@ -36,69 +38,15 @@ namespace Endpoints
             var blogAPIKey = config["DevtoAPIKey"];
 
             builder.Services.AddMediatR(typeof(GetUserInfoQuery).Assembly);
-
-
             builder.Services.AddDbContext<SiteDbContext>(options => options.UseSqlServer(connectionString));
             builder.Services.AddScoped(typeof(IRepository<,>), typeof(EFRepository<,>));
             builder.Services.AddScoped<IRecurringJobService, RecurringJobService>();
-            //services.AddHttpClient();
-            //builder.Services.AddAutofac(autoFacBuilder =>
-            //{
-            //    autoFacBuilder.Populate(builder.Services);
-            //    autoFacBuilder.RegisterModule<ApplicationModule>();
-            //    //autoFacBuilder.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).GetTypeInfo().Assembly });
-            //});
-            //builder.Services.AddHttpClient("dev.to", client =>
-            //{
-            //    client.BaseAddress = new Uri("https://dev.to/");
-            //    client.DefaultRequestHeaders.Add("api-key", blogAPIKey);
-
-            //});
-            //builder.Services.AddHttpClient("github", client =>
-            //{
-            //    client.BaseAddress = new Uri("https://api.github.com");
-            //    client.DefaultRequestHeaders.Add("User-Agent", "Personal-Site");
-            //});
             builder.Services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).GetTypeInfo().Assembly, typeof(InfrastructureProfile).GetTypeInfo().Assembly });
-
-            //builder.Services.AddScoped<GithubMessageHandler>();
-            //builder.Services.AddScoped<BlogPostsMessageHandler>();
-            //builder.Services.AddScoped<VideoPlatformMessageHandler>();
-            //builder.Services.AddScoped<TwitchWebhookSubscriptionHandler>();
-            //builder.Services.AddScoped(typeof(IRepository<,>), typeof(EFRepository<,>));
-
-            //builder.Services.AddScoped<HandlerResolver>(sp => message =>
-            //{
-            //    switch (message)
-            //    {
-            //        case MessageType.GithubRepoUpdate:
-            //            return sp.GetService<GithubMessageHandler>();
-            //        case MessageType.UserBlogPostsUpdate:
-            //            return sp.GetService<BlogPostsMessageHandler>();
-            //        case MessageType.VideoPlatformUpdate:
-            //            return sp.GetService<VideoPlatformMessageHandler>();
-            //        case MessageType.TwitchWebhookSubscription:
-            //            return sp.GetService<TwitchWebhookSubscriptionHandler>();
-            //        default:
-            //            return null;
-
-            //    }
-            //});
-            //builder.servicesRegisterType<GithubRepoServices>().As<IGithubService>();
-            //builder.RegisterType<BlogPostService>().As<IBlogPostService>();
-            //builder.RegisterType<RecurringJobService>().As<IRecurringJobService>();
-            //builder.RegisterType<GithubRepoServices>().As<IGithubService>();
-            //builder.RegisterGeneric(typeof(EFRepository<,>)).As(typeof(IRepository<,>));
-            //builder.RegisterType<GithubMessageHandler>().Keyed<IMessageHandler<IMessage>>(MessageType.GithubRepoUpdate);
-            //builder.RegisterType<BlogPostsMessageHandler>().Keyed<IMessageHandler<IMessage>>(MessageType.UserBlogPostsUpdate);
-
             builder.Services.AddTransient<IGithubService, GithubRepoServices>();
             builder.Services.AddTransient<IBlogPostService, BlogPostService>();
             builder.Services.AddTransient<ITwitchService, TwitchService>();
             builder.Services.AddTransient<IYouTubeService, YouTubeService>();
-            //builder.Services.AddTransient<GithubMessageHandler>();
-            //builder.Services.AddTransient<GithubMessageHandler>();
-            //builder.Services.AddTransient<IMessageHandlerFactory, MessageHandlerFactory>();
+            
         }
     }
 }

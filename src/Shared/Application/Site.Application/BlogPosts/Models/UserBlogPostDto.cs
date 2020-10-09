a6603b0a35e390fb58037fd34f9c73c083f66b35
John Hanbury <johnhanbury4@gmail.com>
2020-10-09 23:53:57 +0100

    added new blog tables

--- src/Shared/Application/Site.Application/BlogPosts/Models/UserBlogPostDto.cs
+++ src/Shared/Application/Site.Application/BlogPosts/Models/UserBlogPostDto.cs
@@ -1,16 +1,23 @@
+using System;
+using System.Collections.Generic;
 using Site.Application.Users.Models;
 
 namespace Site.Application.BlogPosts.Models
 {
-    public class UserBlogPostDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Url { get; set; }
-        public string ImageUrl { get; set; }
-        public string Teaser { get; set; }
-        public string Source { get; set; }
-        public int UserId { get; set; }
-        public string AuthorName { get; set; }
-    }
+  public class UserBlogPostDto
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Url { get; set; }
+    public string ImageUrl { get; set; }
+    public string Teaser { get; set; }
+    public int Likes { get; set; }
+    public int Comments { get; set; }
+    public int Views { get; set; }
+    public DateTime PublishDate { get; set; }
+    public List<string> Tags { get; set; }
+    public string Source { get; set; }
+    public int UserId { get; set; }
+    public string AuthorName { get; set; }
+  }
 }
--- src/Shared/Persistance/Site.Persistance/SiteDbContext.cs
+++ src/Shared/Persistance/Site.Persistance/SiteDbContext.cs
@@ -4,46 +4,47 @@ using Site.Domain.Entities.Audit;
 
 namespace Site.Persistance
 {
-    public class SiteDbContext : DbContext
+  public class SiteDbContext : DbContext
+  {
+    public SiteDbContext(DbContextOptions<SiteDbContext> options)
+          : base(options)
     {
-        public SiteDbContext(DbContextOptions<SiteDbContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<GithubRepo> GithubRepos { get; set; }
-        public DbSet<Address> Addresses { get; set; }
-        public DbSet<SocialMediaAccount> SocialMediaAccounts { get; set; }
-        public DbSet<UserBlogPost> UserBlogPosts { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Hobby> Hobby { get; set; }
-        public DbSet<UserHobby> UserHobby { get; set; }
-        public DbSet<Project> Projects { get; set; }
-        public DbSet<ProjectSkill> ProjectSkills { get; set; }
-        public DbSet<ProjectTechnology> ProjectTechnologies { get; set; }
-        public DbSet<Skill> Skills { get; set; }
-        public DbSet<Technology> Technologies { get; set; }
-        public DbSet<Video> Videos { get; set; }
-        public DbSet<PlatformAccount> PlatformAccounts { get; set; }
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<University> Universities { get; set; }
-        public DbSet<Grade> Grades { get; set; }
-        public DbSet<DegreeType> DegreeTypes { get; set; }
-        public DbSet<Degree> Degrees { get; set; }
-        public DbSet<UserDegree> UserDegrees { get; set; }
-        public DbSet<RoleTechnology> RoleTechnologies { get; set; }
-        public DbSet<RoleSkill> RoleSkills { get; set; }
-        public DbSet<UserWorkExperience> UserWorkExperiences { get; set; }
-        public DbSet<PerformanceLog> PerformanceLogs { get; set; }
-
-
-
-
-        
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SiteDbContext).Assembly);
-        }
     }
+
+    public DbSet<GithubRepo> GithubRepos { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<SocialMediaAccount> SocialMediaAccounts { get; set; }
+    public DbSet<UserBlogPost> UserBlogPosts { get; set; }
+    public DbSet<BlogPostTag> BlogPostTags { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Hobby> Hobby { get; set; }
+    public DbSet<UserHobby> UserHobby { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<ProjectSkill> ProjectSkills { get; set; }
+    public DbSet<ProjectTechnology> ProjectTechnologies { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+    public DbSet<Technology> Technologies { get; set; }
+    public DbSet<Video> Videos { get; set; }
+    public DbSet<PlatformAccount> PlatformAccounts { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<University> Universities { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+    public DbSet<DegreeType> DegreeTypes { get; set; }
+    public DbSet<Degree> Degrees { get; set; }
+    public DbSet<UserDegree> UserDegrees { get; set; }
+    public DbSet<RoleTechnology> RoleTechnologies { get; set; }
+    public DbSet<RoleSkill> RoleSkills { get; set; }
+    public DbSet<UserWorkExperience> UserWorkExperiences { get; set; }
+    public DbSet<PerformanceLog> PerformanceLogs { get; set; }
+
+
+
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfigurationsFromAssembly(typeof(SiteDbContext).Assembly);
+    }
+  }
 }
